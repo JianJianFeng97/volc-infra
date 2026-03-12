0d53a64c013880a587d328374895fa530fe36c71
JianJianFeng97 <jianjianfeng@example.com>
2026-03-12 11:37:28 +0800

    sync current volc resources to dev environment config

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -36,6 +36,35 @@ module "naming" {
 }
 
 # -------------------------------------------------------------------
+# VPC: Default VPC for Shanghai region
+# -------------------------------------------------------------------
+resource "volcengine_vpc" "default" {
+  vpc_name    = "Default"
+  cidr_block  = "172.31.0.0/16"
+  description = "System created default VPC."
+}
+
+# -------------------------------------------------------------------
+# Subnet: Default subnet in Shanghai-A zone
+# -------------------------------------------------------------------
+resource "volcengine_subnet" "default_a" {
+  subnet_name = "Default"
+  cidr_block  = "172.31.0.0/20"
+  zone_id     = "cn-shanghai-a"
+  vpc_id      = volcengine_vpc.default.id
+  description = "System created default Subnet."
+}
+
+# -------------------------------------------------------------------
+# Security Group: Default security group for dev environment
+# -------------------------------------------------------------------
+resource "volcengine_security_group" "default" {
+  security_group_name = "Default"
+  vpc_id              = volcengine_vpc.default.id
+  description         = "默认安全组"
+}
+
+# -------------------------------------------------------------------
 # Outputs (handy for referencing from other tools / scripts)
 # -------------------------------------------------------------------
 output "tags" {
@@ -46,4 +75,19 @@ output "tags" {
 output "name_prefix" {
   description = "Resource name prefix"
   value       = module.naming.name_prefix
+}
+
+output "vpc_default_id" {
+  description = "ID of the default dev VPC"
+  value       = volcengine_vpc.default.id
+}
+
+output "subnet_default_a_id" {
+  description = "ID of the default dev subnet in cn-shanghai-a"
+  value       = volcengine_subnet.default_a.id
+}
+
+output "sg_default_id" {
+  description = "ID of the default dev security group"
+  value       = volcengine_security_group.default.id
 }
